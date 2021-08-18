b20646a1b211c7c9244c66b8e48b7b24a2468597
Maxwell DeMaio <maxdman321@gmail.com>
2021-08-18 19:45:29 -0400

    complete atoi

--- leetcode_java/src/com/eta/StrToIntAtoi.java
+++ leetcode_java/src/com/eta/StrToIntAtoi.java
@@ -2,16 +2,20 @@ package com.eta;
 
 public class StrToIntAtoi {
 	public int myAtoi(String s) {
+		if (s.equals("")) {
+			return 0;
+		}
+		
 		// Initialize sum, sign, and iter variables
 		int res = 0, sign = 1, i = 0;
 		
 		// Go through whitespace
-		while (s.charAt(i) == ' ') {
+		while (i < s.length() && s.charAt(i) == ' ') {
 			i++;
 		}
 		
 		// Check for sign (and increment to keep going)
-		if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+		if (i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
 			if (s.charAt(i++) == '-') {
 				sign = -1;
 			}
@@ -21,8 +25,17 @@ public class StrToIntAtoi {
 		// Only iterate while there is valid input
 		// Make sure the res does not overflow the Integer max/min value
 		while(i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-			
+			// handle int overflow
+			if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > 7)) {
+				if (sign == -1) {
+					return Integer.MIN_VALUE;
+				}
+				if (sign == 1) {
+					return Integer.MAX_VALUE;
+				}
+			}
             res = res * 10 + Character.getNumericValue(s.charAt(i));
+            i++;
 		}
 		
         return sign * res;
--- leetcode_java/src/com/eta/_Tester.java
+++ leetcode_java/src/com/eta/_Tester.java
@@ -7,13 +7,15 @@ public class _Tester {
 		// Access them without creating a new instance of the object
 		
 		// Test cases
+		String test0 = "";
 		String test1 = "42"; //42
 		String test2 = "  -42"; //-42
 		String test3 = "4193 with words"; //4193
 		String test4 = "words and 987"; //0
 		String test5 = "-91283472332"; //-2147483648
+		String test6 = " "; // 0
 		
 		StrToIntAtoi s = new StrToIntAtoi();
-		System.out.println("Return value: " + s.myAtoi(test2));
+		System.out.println("Return value: " + s.myAtoi(test6));
 	}
 }
